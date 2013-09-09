ff7d2c993235415df0a055463236c5b5de8ff9ab
moz <moz@local>
2013-09-09 21:02:15 +0200

    cleanup added checks: non-master can only acces repo with the same name

--- ConfigStore.py
+++ ConfigStore.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# script use to handle multiuser access to a sing git repo.
+# - servers have an ssh key and are able to access their own config repo
+# - "masters" can access all repos
+
 # heavy inspiration from
 # - https://moocode.com/posts/6-code-your-own-multi-user-private-git-server-in-5-minutes
 
@@ -8,24 +12,18 @@ from subprocess import call
 
 # user and permissions are passed from authorized_keys
 
-
-
 # handle cmd line
 user = sys.argv[1]
-print >>sys.stderr, "-", user
-
 permissions = sys.argv[2]
-print >>sys.stderr, "-", permissions
-
 command = os.environ['SSH_ORIGINAL_COMMAND']
-print >>sys.stderr, "-", command
 
 if not user or not permissions or not command:
     sys.exit( 1 )
 
 # check the supplied command contains a valid git action 
 valid_actions = ['git-receive-pack', 'git-upload-pack']
-action = command.split(' ')[0]
+action, repo = command.split(' ')
+repo = repo.strip("'").split('.')[0]
 
 if not action in valid_actions:
     print >>sys.stderr, "bad action:", action
@@ -41,10 +39,15 @@ if action == 'git-receive-pack' and not 'w' in permissions:
     print >>sys.stderr, "write denied for %s"%user
     sys.exit( 4 ) 
 
-print >>sys.stderr, "user %s authorized\n"%user
+if user != 'master' and user != repo:
+    print >>sys.stderr, "user %s is not allowed to access repo %s"%(user, repo)
+    sys.exit( 5 )
+
+print >>sys.stderr, "user %s authorized to access repo %s\n"%(user, repo)
 
 # user made a valid request so handing over to git-shell
-call( ['git', 'shell', '-c', command] )
+params = ['git', 'shell', '-c', "%s '%s.git'"%(action, repo)]
+call( params )
 
 
 
